220c708fc474122febaf851542a3b40579506f2e
Jeevan James <jeevanjj@live.com>
2023-09-07 19:20:17 +0530

    Add a sample project

---
+++ sample/BasicSample/MasterData.cs
@@ -0,0 +1,29 @@
+﻿namespace Jeevan.PolicyEvaluation.Sample.BasicSample;
+
+internal sealed class MasterData
+{
+    internal Dictionary<string, string> Claims { get; } = new(StringComparer.OrdinalIgnoreCase)
+    {
+        ["name"] = "The Flash",
+        ["userid"] = "flash",
+        ["email"] = "flash@justice-league.com",
+        ["age"] = "30",
+    };
+
+    internal Dictionary<string, PolicyExecutorFunc> Policies { get; } = new(StringComparer.OrdinalIgnoreCase)
+    {
+        ["JusticeLeagueMember"] = c => new(
+            c["email"].EndsWith("@justice-league.com", StringComparison.OrdinalIgnoreCase),
+            "The user must have a valid Justice League email ID."),
+        ["BusinessHours"] = _ => new(
+            DateTime.Now is { Hour: >= 8 and <= 17 },
+            "Must be during business hours."),
+        ["YoungEnoughToWorkMore"] = c => new(
+            int.Parse(c["age"]) <= 30,
+            "User must be young enough to work outside office hours (30 years or less)."),
+    };
+}
+
+internal sealed record PolicyResult(bool Success, string ErrorMessage);
+
+internal delegate PolicyResult PolicyExecutorFunc(Dictionary<string, string> claims);
---
+++ sample/BasicSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Jeevan.PolicyEvaluation;
+using Jeevan.PolicyEvaluation.Sample.BasicSample;
+
+const string expression = "(BusinessHours OR YoungEnoughToWorkMore) AND JusticeLeagueMember";
+
+MasterData masterData = new();
+
+PolicyEvaluator pe = new(EvaluatePolicy, builder => builder
+    .CheckPolicyNameWith<MasterData>((name, md) => md.Policies.ContainsKey(name)));
+
+IExpressionEvaluationOutcome outcome = pe.EvaluateExpression(expression, masterData);
+if (outcome.IsNotSatisfied(out string? policyName, out string? failureMessage))
+    Console.WriteLine($"Policy {policyName} failed with message... {failureMessage}");
+else
+    Console.WriteLine("Passed");
+
+static IPolicyOutcome EvaluatePolicy(string policyName, object? state)
+{
+    if (state is not MasterData md)
+        return PolicyOutcome.Error("Invalid state");
+
+    if (!md.Policies.TryGetValue(policyName, out PolicyExecutorFunc? policy))
+        return PolicyOutcome.InvalidPolicyName(policyName);
+
+    (bool result, string message) = policy(md.Claims);
+    return result ? PolicyOutcome.Pass : PolicyOutcome.Fail(policyName, message);
+}
